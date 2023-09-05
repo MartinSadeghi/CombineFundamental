0aae2df3650a84817e322adf64540e53d0122793
moMMo Sadeghi <37694875+moMMoSadeghi@users.noreply.github.com>
2023-09-05 12:38:07 +0200

    Initial Commit

---
+++ CombineFundamental/CombineFundamentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineFundamentalApp.swift
+//  CombineFundamental
+//
+//  Created by Mommo Sadeghi on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineFundamentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
