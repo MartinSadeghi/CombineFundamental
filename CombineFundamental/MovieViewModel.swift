da5dff305af019cb75905849a2c8c332676a9118
moMMo Sadeghi <37694875+moMMoSadeghi@users.noreply.github.com>
2023-09-08 12:04:21 +0200

    MovieViewModel added.

--- CombineFundamental/MovieViewModel.swift
+++ CombineFundamental/MovieViewModel.swift
@@ -6,4 +6,26 @@
 //
 
 import Foundation
+import Combine
 
+final class MovieViewModel: ObservableObject {
+    
+    @Published var movies: [Movie] = []
+    var cancellables = Set<AnyCancellable>()
+    
+    func fetchInitialData() {
+        fetchMovies()
+            .map(\.results)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .finished:()
+                case .failure(let err):
+                    print(err.localizedDescription)
+                }
+            } receiveValue: { [weak self] movies in
+                self?.movies = movies
+            }
+            .store(in: &cancellables)
+    }
+}
